3fddd928a895150f5c9d454ff3aec7530224b24d
Ivan Magda <imagda15@gmail.com>
2017-02-22 23:11:22 +0900

    chore: Add TensorFlow epochs using sample code
    Cover epochs in TensorFlow and how to choose the right number of epochs.

---
+++ week-5/Intro_to_TensorFlow/epochs/batches.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+import math
+
+
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    # TODO: Implement batching
+    outout_batches = []
+    
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        outout_batches.append(batch)
+        
+    return outout_batches
