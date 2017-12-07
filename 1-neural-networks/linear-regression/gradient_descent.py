27f43af6535caee52e9073503070e8f3402a4a78
Ivan Magda <imagda15@gmail.com>
2017-12-07 23:02:16 +0300

    chore: Gradient descent the code

---
+++ 1-neural-networks/linear-regression/gradient_descent.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+# Weight update can be calculated as:
+# Δwi = ηδxi
+#
+# with the error term δ as:
+# δ = (y − y^) f′(h) = (y − y^)f′(∑wixi)
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_prime(x):
+    """
+    # Derivative of the sigmoid function
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
+
+learnrate = 0.5
+x = np.array([1, 2, 3, 4])
+y = np.array(0.5)
+
+# Initial weights
+w = np.array([0.5, -0.5, 0.3, 0.1])
+
+### Calculate one gradient descent step for each weight
+### Note: Some steps have been consilated, so there are
+###       fewer variable names than in the above sample code
+
+# TODO: Calculate the node's linear combination of inputs and weights
+h = np.dot(w, x)
+
+# TODO: Calculate output of neural network
+nn_output = sigmoid(h)
+
+# TODO: Calculate error of neural network
+error = y - nn_output
+
+# TODO: Calculate the error term
+#       Remember, this requires the output gradient, which we haven't
+#       specifically added a variable for.
+error_term = error * sigmoid_prime(h)
+
+# TODO: Calculate change in weights
+del_w = learnrate * error_term * x
+
+print('Neural Network output:')
+print(nn_output)
+print('Amount of Error:')
+print(error)
+print('Change in Weights:')
+print(del_w)
